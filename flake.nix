6274cc1615a254c60067ce86e61cdbe5b8837afe
Ben Hall <rcmast3r1@gmail.com>
2024-02-22 22:17:16 -0500

    working flake on x86 linux

--- flake.nix
+++ flake.nix
@@ -3,55 +3,47 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs }:
-    let
-      cloud_webserver_overlay = final: prev: {
-        cloud_webserver_pkg = final.callPackage ./default.nix { };
-      };
-      my_overlays = [ cloud_webserver_overlay ];
-      pkgs = import nixpkgs {
-        system = "x86_64-linux";
-        overlays = [ self.overlays.default ];
-      };
 
-      shared_shell = pkgs.mkShell rec {
-        # Update the name to something that suites your project.
-        name = "nix-devshell";
-        packages = with pkgs; [
-          # Development Tools
-          mongodb
-          mongosh
-        ];
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" ] (system:
+      let
+        cloud_webserver_overlay = final: prev: {
+          cloud_webserver_pkg = final.callPackage ./default.nix { };
+        };
 
-        # Setting up the environment variables you need during
-        # development.
-        shellHook =
-          let
-            icon = "f121";
-          in
-          ''
-            export PS1="$(echo -e '\u${icon}') {\[$(tput sgr0)\]\[\033[38;5;228m\]\w\[$(tput sgr0)\]\[\033[38;5;15m\]} (${name}) \\$ \[$(tput sgr0)\]"
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            allowUnfree = true;
+            # Include any other global Nixpkgs configuration here
+          };
+          overlays = [ cloud_webserver_overlay ];
+        };
+
+        shared_shell = pkgs.mkShell rec {
+          name = "nix-devshell";
+          packages = with pkgs; [
+            mongodb
+            mongosh
+          ];
+
+          shellHook = ''
+            export PS1="\u${"f121"} \w (${name}) \$ "
           '';
-      };
-      shared_pkgs = rec {
-        cloud_webserver_pkg = pkgs.cloud_webserver_pkg;
-        default = cloud_webserver_pkg;
-      };
-    in
-    {
-      overlays.default = nixpkgs.lib.composeManyExtensions my_overlays;
+        };
 
-      packages.x86_64-linux = shared_pkgs;
-      packages.aarch64-darwin = shared_pkgs;
-      packages.x86_64-darwin = shared_pkgs;
+      in
+      {
+        packages = rec {
+          cloud_webserver_pkg = pkgs.cloud_webserver_pkg;
+          default = cloud_webserver_pkg;
+        };
 
+        devShells = {
+          default = shared_shell;
+        };
 
-
-      devShells.x86_64-linux.default = shared_shell;
-      devShells.aarch64-darwin.default = shared_shell;
-      devShells.x86_64-darwin.default = shared_shell;
-
-
-    };
+      });
 }
