356cbb62a46698c8af5e35d0390f4283c8d67d0b
sreekara <sreedansd@gmail.com>
2024-02-27 20:11:24 -0500

    feat(db): added basic skeleton for adding to mongodb

--- cloud_webserver/app.py
+++ cloud_webserver/app.py
@@ -1,24 +1,30 @@
+from typing import Mapping, Any
+
 from flask import Flask, request
 from pymongo import MongoClient
+from pymongo.collection import Collection
 from werkzeug.datastructures import FileStorage
 
 import upload
+import db
 
 app = Flask(__name__) 
 
-# root route 
+# root route
 @app.route('/') 
 def hello_world() -> str:
 	return 'Hello, World!'
 
 # Set up MongoDB connection and collection 
-client = MongoClient('mongodb://localhost:27017/') 
+db_client = MongoClient('mongodb://localhost:27017/')
 
 # Create database named demo if they don't exist already 
-db = client['demo'] 
+db = db_client['demo']
 
 # Create collection named data if it doesn't exist already 
-collection = db['data'] 
+demo_collection = db['data']
+meta_data_collection: Collection[Mapping[str, Any]] = db['metadata']
+car_setup_collection: Collection[Mapping[str, Any]] = db['car_setup']
 
 # Add data to MongoDB route 
 @app.route('/add_data', methods=['POST']) 
@@ -27,16 +33,27 @@ def add_data() -> str:
 	data = request.json 
 
 	# Insert data into MongoDB 
-	collection.insert_one(data) 
+	demo_collection.insert_one(data)
 
 	return 'Data added to MongoDB'
 
 
-@app.route('/upload_mcap', methods=['POST'])
-def upload_mcap() -> str:
+@app.route('/save_mcap', methods=['POST'])
+def save_mcap() -> str:
 	if 'file' in request.files:
 		try:
-			upload.save_mcap_file(request.files['file'])
+			path_to_file: str = upload.save_mcap_file(request.files['file'])
+			if path_to_file != "":
+
+				# Need to access and parse the mcap file
+				# Once we know what data is in the mcap file, we can begin to parse it
+
+				meta_data = {}
+				car_setup = {}
+
+				db.save_metadata(path_to_file,
+								 meta_data_collection,
+								 car_setup_collection)
 		except ValueError as e:
 			return 'fail: ' + str(e)
 
