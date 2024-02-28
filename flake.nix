fa886e5b137f122bea72d47a3d0d80f112f096a3
BANANAPEEL202 <bbob52645@gmail.com>
2024-02-27 22:30:49 -0500

    did stuff
    package docker-compose.yml (package.nix)
    updated flake to include the package and docker
    added a module that uses systemd to start the docker

--- flake.nix
+++ flake.nix
@@ -5,7 +5,6 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     flake-utils.url = "github:numtide/flake-utils";
     mcap-protobuf.url = "github:RCMast3r/mcap-protobuf-support-flake";
-
     mcap.url = "github:RCMast3r/py_mcap_nix";
   };
 
@@ -16,15 +15,35 @@
           cloud_webserver_pkg = final.callPackage ./default.nix { };
         };
 
+        yaml_overlay = final: prev: {
+          yaml_pkg = final.callPackage ./package.nix { };
+        };
+
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [ cloud_webserver_overlay mcap-protobuf.overlays.default mcap.overlays.default ];
+          overlays = [ cloud_webserver_overlay mcap-protobuf.overlays.default mcap.overlays.default yaml_overlay ];
+        };
+
+        # Add Docker to the system configuration
+        config = {
+          users.users.nixos.extraGroups = [ "docker" ];
+          services.docker = {
+            enable = true;
+            rootless = {
+              enable = true;
+              setSocketVariable = true;
+            };     
+          };
+          modules = [
+            ./modules/docker.nix
+          ];
         };
 
         shared_shell = pkgs.mkShell rec {
           name = "nix-devshell";
           packages = with pkgs; [
             cloud_webserver_pkg
+            yaml_pkg
           ];
 
           shellHook = ''
@@ -37,6 +56,9 @@
         packages = rec {
           cloud_webserver_pkg = pkgs.cloud_webserver_pkg;
           default = cloud_webserver_pkg;
+
+          yaml_pkg = pkgs.yaml_pkg;
+
         };
 
         devShells = {
