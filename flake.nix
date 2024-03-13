4c70053ed1edf82af9a68c5878275c7485029934
BANANAPEEL202 <bbob52645@gmail.com>
2024-03-12 22:08:17 -0400

    fixed docker

--- flake.nix
+++ flake.nix
@@ -2,19 +2,19 @@
   description = "foxglove websocket protocol python library";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
-    flake-utils.url = "github:numtide/flake-utils";
-    mcap-protobuf.url = "github:RCMast3r/mcap-protobuf-support-flake";
-    mcap.url = "github:RCMast3r/py_mcap_nix";
+  nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+  flake-utils.url = "github:numtide/flake-utils";
+  mcap-protobuf.url = "github:RCMast3r/mcap-protobuf-support-flake";
+  mcap.url = "github:RCMast3r/py_mcap_nix";
 
-    nixos-generators = {
+  nixos-generators = {
       url = "github:nix-community/nixos-generators";
       inputs.nixpkgs.follows = "nixpkgs";
-    };
+  };
   };
 
   outputs = { self, nixpkgs, flake-utils, mcap-protobuf, mcap, nixos-generators, ... }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" ] (system:
+  flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" ] (system:
       
       let
         cloud_webserver_overlay = final: prev: {
@@ -78,21 +78,21 @@
           default = shared_shell;
         };        
       });
-}
+  }
 */
 /*
-{
+  {
   description = "foxglove websocket protocol python library";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
-    flake-utils.url = "github:numtide/flake-utils";
-    mcap-protobuf.url = "github:RCMast3r/mcap-protobuf-support-flake";
-    mcap.url = "github:RCMast3r/py_mcap_nix";
+  nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+  flake-utils.url = "github:numtide/flake-utils";
+  mcap-protobuf.url = "github:RCMast3r/mcap-protobuf-support-flake";
+  mcap.url = "github:RCMast3r/py_mcap_nix";
   };
 
   outputs = { self, nixpkgs, flake-utils, mcap-protobuf, mcap, ... }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" ] (system:
+  flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" ] (system:
       let
         cloud_webserver_overlay = final: prev: {
           cloud_webserver_pkg = final.callPackage ./default.nix { };
@@ -149,8 +149,7 @@
         };
 
       });
-}
-
+  }
 */
 
 {
@@ -165,65 +164,75 @@
 
   outputs = { self, nixpkgs, flake-utils, mcap-protobuf, mcap, ... }: {
     cloud_webserver_overlay = final: prev: {
-          cloud_webserver_pkg = final.callPackage ./default.nix {};
-        };
+      cloud_webserver_pkg = final.callPackage ./default.nix { };
+    };
 
-        yaml_overlay = final: prev: {
-          yaml_pkg = final.callPackage ./package.nix {};
-        };
-        
-      nixosConfigurations.ec2 = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
+    yaml_overlay = final: prev: {
+      yaml_pkg = final.callPackage ./package.nix { };
+    };
+
+    dockerService = nixpkgs.buildEnv {
+      name = "docker-service";
+      buildInputs = [ ./docker.nix ];
+    };
+
+    nixosConfigurations.ec2 = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
       
-       
-       modules =
-        [{ 
-          imports = [./modules/docker.nix];
+
+
+      modules =
+        [{
+          imports = [ ./modules/docker.nix ];
 
           config = {
-              nixpkgs.overlays = [ self.cloud_webserver_overlay mcap-protobuf.overlays.default mcap.overlays.default self.yaml_overlay];
-          users.users.nixos.extraGroups = [ "docker" ];
-
-          #this unused code gets rid of the The ‘fileSystems’ option does not specify your root file system error
-          fileSystems."/" =
-    { device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888";
-      fsType = "ext4";
-    };
-    #this unused code gets rid of this error:
-    #You must set the option ‘boot.loader.grub.devices’ or 'boot.loader.grub.mirroredBoots' to make the system bootable.
-    #https://discourse.nixos.org/t/configure-grub-on-efi-system/2926/2
-   boot.loader.grub.devices = [ "nodev" ];
-   #the following user code also makes errors go away...
-   users.users.nixos.isSystemUser = true;
-    users.groups.nixos = {};
-   users.users.nixos.group = "nixos";
-   services.openssh = {
-  enable = true;
-  # require public key authentication for better security
-  settings.PasswordAuthentication = false;
-  settings.KbdInteractiveAuthentication = false;
-  #settings.PermitRootLogin = "yes";
-};
-   users.users.root.openssh.authorizedKeys.keys = [
-    # your own SSH public key.
-    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC8NWlt5zLQzQbAQ/IYOAcSmwxBUCzwL+iE5yZBQQWNa43ny+aLTKyZsb8S4gywuRT9/pqJELOzGBEKVaoW08tSy48U2/8fkLbJEwRQKIdb3cmvc6xu9dridVHVeETMULwGq99YaFhRDtUAI/d+tsseqGupmDV+/XgVP9UKN3jpa21fmpCNUn3z3lbbPdjRLLbdvphS/PTHAIBsgFxHQ7GAlBO0WhC39pEyyoWrQD+ip7r8iFUg5Tjd/hTH0zJPojZ/kC8DSxjyEQfX3zsJ//PZzf7p7gtbUSAAFNeMe7CKC0Y3ThnBaM/CKTnjH9tqmqTI6BYMaiawnm6ZwsM3oAb3ZpIR7vnhkr45jKzpKZJogid5cVdNLnTBoPd/I9nB0z/F79L9CQ8vFeag7Py0TxQJUvdXcxAVlgpyFEfsMCnf3VJDW1afLfc/dxhSyUyVqqEU1qCdzB6oKUHNgKv7RI2yzNyKEuh9HOVVn/4b/xDua4MYacYVYwKD60IPA4hgXzc= home@lawn-128-61-50-84.lawn.gatech.edu"
-  ];
-          };
-          
-          
-          options = {
-             services.docker = {
-            enable = true;
-            rootless = {
+            virtualisation.docker.enable = true;
+            nixpkgs.config.allowUnfree = true; 
+            nixpkgs.overlays = [ self.cloud_webserver_overlay mcap-protobuf.overlays.default mcap.overlays.default self.yaml_overlay ];
+            users.users.nixos.extraGroups = [ "docker" ];
+            
+            
+            #this unused code gets rid of the The ‘fileSystems’ option does not specify your root file system error
+            fileSystems."/" =
+              {
+                device = "/dev/xvda";
+                fsType = "ext4";
+              };
+            #this unused code gets rid of this error:
+            #You must set the option ‘boot.loader.grub.devices’ or 'boot.loader.grub.mirroredBoots' to make the system bootable.
+            #https://discourse.nixos.org/t/configure-grub-on-efi-system/2926/2
+            boot.loader.grub.devices = [ "nodev" ];
+            #the following user code also makes errors go away...
+            users.users.nixos.isSystemUser = false;
+            users.users.nixos.isNormalUser = true;
+            #users.groups.nixos = { };
+            #users.users.nixos.group = "nixos";
+            services.openssh = {
               enable = true;
-              setSocketVariable = true;
-            };     
+              # require public key authentication for better security
+              settings.PasswordAuthentication = false;
+              settings.KbdInteractiveAuthentication = false;
+              #settings.PermitRootLogin = "yes";
+            };
+            users.users.nixos.openssh.authorizedKeys.keys = [
+              # your own SSH public key.
+              "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC8NWlt5zLQzQbAQ/IYOAcSmwxBUCzwL+iE5yZBQQWNa43ny+aLTKyZsb8S4gywuRT9/pqJELOzGBEKVaoW08tSy48U2/8fkLbJEwRQKIdb3cmvc6xu9dridVHVeETMULwGq99YaFhRDtUAI/d+tsseqGupmDV+/XgVP9UKN3jpa21fmpCNUn3z3lbbPdjRLLbdvphS/PTHAIBsgFxHQ7GAlBO0WhC39pEyyoWrQD+ip7r8iFUg5Tjd/hTH0zJPojZ/kC8DSxjyEQfX3zsJ//PZzf7p7gtbUSAAFNeMe7CKC0Y3ThnBaM/CKTnjH9tqmqTI6BYMaiawnm6ZwsM3oAb3ZpIR7vnhkr45jKzpKZJogid5cVdNLnTBoPd/I9nB0z/F79L9CQ8vFeag7Py0TxQJUvdXcxAVlgpyFEfsMCnf3VJDW1afLfc/dxhSyUyVqqEU1qCdzB6oKUHNgKv7RI2yzNyKEuh9HOVVn/4b/xDua4MYacYVYwKD60IPA4hgXzc= home@lawn-128-61-50-84.lawn.gatech.edu"
+            ];
           };
-          
+
+
+          options = {
+            services.docker = {
+              enable = true;
+              rootless = {
+                enable = true;
+                setSocketVariable = true;
+              };
+            };
+
           };
-          
-        }
-        ];
+
+        }];
     };
 
   };
