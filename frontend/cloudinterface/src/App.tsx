d116d3192c86e10a26248f8bb84d59aacaa02467
Abby M <abbyvmartin51@gmail.com>
2024-03-26 20:45:44 -0400

    add mantine library

--- frontend/cloudinterface/src/App.tsx
+++ frontend/cloudinterface/src/App.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// Import styles of packages that you've installed.
+// All packages except `@mantine/hooks` require styles imports
+import '@mantine/core/styles.css';
 
-export default App;
+import { MantineProvider } from '@mantine/core';
+
+export default function App() {
+  return <MantineProvider>{
+      <p>test</p>
+    }</MantineProvider>;
+}
