735f9f8803b93cfed2492cd18cf3999d4a3abd6a
sreekara <sreedansd@gmail.com>
2024-07-25 00:42:13 -0400

    feat(cloud_webserver): init v2 of cloud webserver

---
+++ cloud_webserver_v2/cmd/cloud_webserver_v2/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	// Simple middleware stack
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	// Set a timeout value on the request context (ctx), that will signal
+	// through ctx.Done() that the request has timed out and further
+	// processing should be stopped.
+	r.Use(middleware.Timeout(2 * time.Minute))
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome"))
+	})
+	http.ListenAndServe(":8080", r)
+}
---
+++ cloud_webserver_v2/go.mod
@@ -0,0 +1,5 @@
+module github.com/hytech-racing/cloud-webserver-v2
+
+go 1.22.3
+
+require github.com/go-chi/chi/v5 v5.1.0 // indirect
