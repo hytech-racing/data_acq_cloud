6eb947e8e73134d43f7c3bdd81473de93d19a5b5
sreekara <sreedansd@gmail.com>
2024-10-28 00:43:52 -0400

    refactor(mcap_handler): fixed single file upload route to mimic the batch upload route

--- cloud_webserver_v2/internal/delivery/http/mcap_handler.go
+++ cloud_webserver_v2/internal/delivery/http/mcap_handler.go
@@ -2,11 +2,7 @@ package http
 
 import (
 	"context"
-	"errors"
-	"fmt"
-	"io"
 	"log"
-	"math"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -62,109 +58,27 @@ It currently:
   - creates a raw (no calculations performed onto it) MATLAB file
 */
 func (h *mcapHandler) UploadMcap(w http.ResponseWriter, r *http.Request) {
-	ctx := r.Context()
+	if err := r.ParseMultipartForm(32 << 20); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	defer r.MultipartForm.RemoveAll()
 
-	// Handles the file input from the HTTP Request
-	err := r.ParseMultipartForm(int64(math.Pow(10, 9)))
+	file := r.MultipartForm.File["file"]
+	jobIds := make([]string, 1, len(file))
+	fileHeader := file[0]
+	job, err := h.fileProcessor.QueueFile(fileHeader)
 	if err != nil {
-		fmt.Errorf("cloud not parse mutlipart form")
+		log.Printf("Failed to queue file %s: %v", fileHeader.Filename, err)
+		return
 	}
+	jobIds[0] = job.ID
 
-	file, handler, err := r.FormFile("file")
-	if err != nil {
-		fmt.Errorf("could not get the mcap file")
-	}
-	defer file.Close()
+	response := make(map[string]interface{})
+	response["message"] = "created file processing job"
+	response["data"] = jobIds
 
-	log.Printf("Uploaded file: %+v", handler.Filename)
-	log.Printf("File size: %+v", handler.Size)
-	log.Printf("MIME Header: %+v", handler.Header)
-
-	mcapUtils := utils.NewMcapUtils()
-
-	reader, err := mcapUtils.NewReader(file)
-	if err != nil {
-		fmt.Errorf("could not create mcap reader")
-	}
-
-	schemaList, err := mcapUtils.GetMcapSchemaList(reader)
-	if err != nil {
-		log.Panicf("%v", err)
-	}
-
-	message_iterator, err := reader.Messages()
-	if err != nil {
-		fmt.Errorf("could not get mcap mesages")
-	}
-
-	// This is all the subsribers relavent to this POST request. You can attach more workers here if need be.
-	subscriberMapping := make(map[string]messaging.SubscriberFunc)
-	subscriberMapping["print"] = messaging.PrintMessages
-	// subscriberMapping["vn_plot"] = messaging.PlotLatLon
-	// subscriberMapping["matlab_writer"] = messaging.CreateInterpolatedMatlabFile
-
-	publisher := messaging.NewPublisher(true)
-	subscriber_names := make([]string, len(subscriberMapping))
-	idx := 0
-	for subscriber_name, function := range subscriberMapping {
-		subscriber_names[idx] = subscriber_name
-		publisher.Subscribe(idx+1, subscriber_name, function)
-		idx++
-	}
-
-	go func() {
-		// Some subscribers may need specfic information before being able to perform their tasks. For example, (CreateInterpolatedMatlabFile)
-		// Because of this, they will need their first message to set paramaters. This is what initMessage is for.
-		initMessage := make(map[string]interface{})
-		initMessage["schema_list"] = schemaList
-		h.routeMessagesToSubscribers(ctx, publisher, &utils.DecodedMessage{Topic: messaging.INIT, Data: initMessage}, &subscriber_names)
-
-		for {
-			schema, channel, message, err := message_iterator.NextInto(nil)
-
-			// Checks if we have no more messages to read from the MCAP. If so, it lets the subscribers know
-			if errors.Is(err, io.EOF) {
-				h.routeMessagesToSubscribers(ctx, publisher, &utils.DecodedMessage{Topic: messaging.EOF}, &subscriber_names)
-				break
-			}
-
-			if err != nil {
-				log.Fatalf("error reading mcap message: %v", err)
-			}
-
-			if schema == nil {
-				log.Printf("no schema found for channel ID: %d, channel: %v", message.ChannelID, channel)
-				continue
-			}
-
-			decodedMessage, err := mcapUtils.GetDecodedMessage(schema, message)
-			if err != nil {
-				log.Printf("could not decode message: %v", err)
-			}
-
-			h.routeMessagesToSubscribers(ctx, publisher, decodedMessage, &subscriber_names)
-		}
-
-		// Need to make sure to close the subscribers or our code will hang and wait forever
-		publisher.CloseAllSubscribers()
-	}()
-
-	publisher.WaitForClosure()
-
-	// subscriberResults := publisher.GetResults()
-	// interpolatedData := subscriberResults["matlab_writer"].ResultData["interpolated_data"]
-	// utils.CreateMatlabFile(interpolatedData.(*map[string]map[string][]float64))
-
-	// Logic to get all the misc. information
-
-	//TODO:
-	/*
-		Upload files to the AWS s3_repository
-		Compile all data to store in MongoDB database
-		Cleanup
-	*/
-
-	fmt.Println("All Subscribers finished")
+	render.JSON(w, r, response)
 }
 
 func (h *mcapHandler) routeMessagesToSubscribers(ctx context.Context, publisher *messaging.Publisher, decodedMessage *utils.DecodedMessage, allNames *[]string) {
