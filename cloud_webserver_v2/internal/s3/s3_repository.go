aa64e278ad9c6f2c963813f92fb65a20f2d1d418
Anuj Naik <41352184+AnujNaik@users.noreply.github.com>
2024-11-14 19:30:51 -0500

    s3_test working, tests by writing, reading, then deleting txt file

--- cloud_webserver_v2/internal/s3/s3_repository.go
+++ cloud_webserver_v2/internal/s3/s3_repository.go
@@ -38,6 +38,20 @@ func (s *S3Repository) WriteObject(ctx context.Context, writer *io.WriterTo, obj
 	}
 }
 
+func (s *S3Repository) WriteObjectReader(ctx context.Context, reader io.Reader, objectName string) error {
+	_, err := s.s3_session.client.PutObject(ctx, &s3.PutObjectInput{
+		Bucket: aws.String(s.s3_session.bucket),
+		Key:    aws.String(objectName),
+		Body:   reader,
+	})
+	if err != nil {
+		return fmt.Errorf("Couldn't upload file %v to %v:%v. Here's why: %v\n",
+			objectName, s.s3_session.bucket, objectName, err)
+	}
+
+	return nil
+}
+
 func (s *S3Repository) ListObjects(ctx context.Context) {
 	result, err := s.s3_session.client.ListBuckets(ctx, &s3.ListBucketsInput{})
 	if err != nil {
@@ -60,4 +74,32 @@ func (s *S3Repository) GetSignedUrl(ctx context.Context, bucket string, objectPa
 	}
 
 	return request.URL
+}
+
+func (s *S3Repository) FileExists(ctx context.Context, objectName string) (bool, error) {
+	_, err := s.s3_session.client.HeadObject(ctx, &s3.HeadObjectInput{
+		Bucket: aws.String(s.s3_session.bucket),
+		Key:    aws.String(objectName),
+	})
+
+	if err != nil {
+		return false, fmt.Errorf("error checking if file %v exists in bucket %v: %v", objectName, s.s3_session.bucket, err)
+	}
+
+	return true, nil
+}
+
+func (s *S3Repository) DeleteObject(ctx context.Context, objectName string) (bool, error) {
+	input := &s3.DeleteObjectInput{
+		Bucket: aws.String(s.s3_session.bucket), // s3 bucket name
+		Key:    aws.String(objectName),          // file name
+	}
+
+	// Delete the object
+	_, err := s.s3_session.client.DeleteObject(ctx, input)
+	if err != nil {
+		return false, fmt.Errorf("error deleting file named %v in bucket %v: %v", objectName, s.s3_session.bucket, err)
+	}
+
+	return true, nil
 }
---
+++ cloud_webserver_v2/internal/s3/s3_test.go
@@ -0,0 +1,78 @@
+package s3
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	"github.com/joho/godotenv"
+)
+
+func TestS3(t *testing.T) {
+	t.Log("Starting tests...")
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		t.Errorf("Error loading .env file %s", err)
+	}
+
+	// Setup aws s3 connection
+	aws_region := os.Getenv("AWS_REGION")
+	if aws_region == "" {
+		t.Errorf("could not get aws region environment variable")
+	}
+
+	aws_bucket := os.Getenv("AWS_S3_RUN_BUCKET")
+	if aws_bucket == "" {
+		t.Errorf("could not get aws run bucket environment variable")
+	}
+
+	awsAccessKey := os.Getenv("AWS_ACCESS_KEY")
+	if awsAccessKey == "" {
+		t.Errorf("could not get aws access key environment variable")
+	}
+
+	awsSecretKey := os.Getenv("AWS_SECRET_KEY")
+	if awsSecretKey == "" {
+		t.Errorf("could not get aws secret key environment variable")
+	}
+
+	s3_respository := NewS3Session(awsAccessKey, awsSecretKey, aws_region, aws_bucket)
+
+	var ctx = context.Background()
+	file, err := os.Open("test.txt")
+	if err != nil {
+		t.Errorf("Error opening file %s", err)
+	}
+
+	// Write the object to S3
+	obj_name := "s3_test_obj"
+
+	err = s3_respository.WriteObjectReader(ctx, file, obj_name)
+	if err != nil {
+		t.Errorf("Failed to write object to S3: %v", err)
+		return
+	}
+
+	t.Log("Successfully wrote object to S3")
+
+	exists, err := s3_respository.FileExists(ctx, obj_name)
+	if err != nil {
+		t.Errorf("File doesn't exist: %v", err)
+	}
+
+	deleted, err := s3_respository.DeleteObject(ctx, obj_name)
+	if err != nil {
+		t.Errorf("Unable to delete file: %v", err)
+		return
+	}
+
+	want := true
+	got := exists && deleted
+
+	if got != want {
+		t.Errorf("Test fail! want: '%t', got: '%t'", want, got)
+	}
+
+	t.Log("Successfully deleted object from S3")
+}
