4d49cbf59a5a0992d1c04051dfeab7b554e921c2
Ryan Lau <47727459+ryanlau@users.noreply.github.com>
2025-03-19 21:54:34 -0400

    add docs

--- cloud_webserver_v2/internal/mps/mps.go
+++ cloud_webserver_v2/internal/mps/mps.go
@@ -10,7 +10,10 @@ import (
 )
 
 type MatlabClient struct {
-	mpsBaseUrl     string
+	// URI of the MPS server
+	mpsBaseUrl string
+
+	// Contains the ids of all the jobs submitted and are being processed/queued
 	jobsProcessing []string
 }
 
@@ -25,33 +28,55 @@ const (
 	CANCELLED  matlabJobState = "CANCELLED"
 )
 
+// Represents HTTP response of an MPS async job request
+// https://www.mathworks.com/help/mps/restfuljson/postasynchronousrequest.html
 type matlabJobResponse struct {
-	ID              string         `json:"id"`
-	Self            string         `json:"self"`
-	Up              string         `json:"up"`
-	LastModifiedSeq int            `json:"lastModifiedSeq"`
-	State           matlabJobState `json:"state"`
-	Client          string         `json:"client"`
+	// ID of a particular request
+	ID string `json:"id"`
+
+	// URI of particular request
+	Self string `json:"self"`
+
+	// URI used to view a collection of requests on the server
+	Up string `json:"up"`
+
+	// Number indicating when a request represented by self was last modified
+	LastModifiedSeq int `json:"lastModifiedSeq"`
+
+	// State of a request
+	State matlabJobState `json:"state"`
+
+	// Client id or name that was specified as a query parameter while initiating a request
+	Client string `json:"client"`
 }
 
+// Represents HTTP response of the result of a MPS job request
+// https://www.mathworks.com/help/mps/restfuljson/getresultofrequest.html
 type matlabJobResult struct {
-	LHS []struct {
-		Mwdata []string `json:"mwdata"`
-		Mwsize []int    `json:"mwsize"`
-		Mwtype string   `json:"mwtype"`
-	} `json:"lhs"`
+	// LHS resprents all the results calculated
+	// For our purposes, LHS will always be an array with 1 JSON string.
+	LHS []string `json:"lhs"`
 }
 
+// Represents the HTTP request payload of an MPS async job request
+// https://www.mathworks.com/help/mps/restfuljson/postasynchronousrequest.html
 type matlabJobRequestPayload struct {
-	Nargout      int      `json:"nargout"`
-	RHS          []string `json:"rhs"`
+	// Number of outputs that the client application is requesting from the deployed MATLAB® function.
+	Nargout int `json:"nargout"`
+
+	// Input arguments to the deployed MATLAB function, specified as an array of strings
+	// For our purposes, RHS will be an array of 1 string which is the path to an .h5 file
+	RHS []string `json:"rhs"`
+
+	// Specify the notation of the MATLAB output response.
+	// We always use the "small" mode and the "string" nanInfFormat.
 	OutputFormat struct {
 		Mode         string `json:"mode"`
 		NanInfFormat string `json:"nanInfFormat"`
 	} `json:"outputFormat"`
 }
 
-func NewMatlabJobRequestPayload(rhs []string) *matlabJobRequestPayload {
+func newMatlabJobRequestPayload(rhs []string) *matlabJobRequestPayload {
 	return &matlabJobRequestPayload{
 		Nargout: 1,
 		RHS:     rhs,
@@ -59,8 +84,8 @@ func NewMatlabJobRequestPayload(rhs []string) *matlabJobRequestPayload {
 			Mode         string `json:"mode"`
 			NanInfFormat string `json:"nanInfFormat"`
 		}{
-			Mode:         "async",
-			NanInfFormat: "text",
+			Mode:         "small",
+			NanInfFormat: "string",
 		},
 	}
 }
@@ -84,6 +109,7 @@ func NewMatlabClient(mpsBaseUrl string) *MatlabClient {
 	}
 }
 
+// Enables the poll for result loop
 func (m *MatlabClient) PollForResults() {
 	go m.pollForResults()
 }
@@ -110,7 +136,7 @@ func (m *MatlabClient) pollForResults() {
 
 			if data.State == READY {
 				m.processResult(job)
-				m.DeleteMatlabJobResult(job)
+				m.deleteMatlabJobResult(job)
 			} else {
 				newJobsProcessing = append(newJobsProcessing, job)
 			}
@@ -145,10 +171,12 @@ func (m *MatlabClient) processResult(jobId string) {
 		log.Fatalf("error unmarshalling response body: %v", err)
 	}
 
-	log.Printf("result for mps job %s: %s", jobId, data.LHS[0].Mwdata[0])
+	log.Printf("result for mps job %s: %s", jobId, data.LHS[0])
 }
 
-func (m *MatlabClient) DeleteMatlabJobResult(jobId string) {
+// Removes the job as well as the job result from the MPS.
+// https://www.mathworks.com/help/mps/restfuljson/deleterequest.html
+func (m *MatlabClient) deleteMatlabJobResult(jobId string) {
 	req, err := http.NewRequest("DELETE", m.mpsBaseUrl+jobId, nil)
 
 	if err != nil {
@@ -170,9 +198,12 @@ func (m *MatlabClient) DeleteMatlabJobResult(jobId string) {
 	log.Printf("deleted mps job result %s", jobId)
 }
 
+// Submits a new synchronous job to the MPS.
+// The MPS client will save the job id and wait for the result and process it in the background
+// https://www.mathworks.com/help/mps/restfuljson/postasynchronousrequest.html
 func (m *MatlabClient) SubmitMatlabJob(h5FileName string, packageName string, functionName string) {
 	log.Println("submitting matlab job")
-	payload := NewMatlabJobRequestPayload([]string{"/home/hytech/" + h5FileName})
+	payload := newMatlabJobRequestPayload([]string{"/home/hytech/" + h5FileName})
 	payloadJson, err := json.Marshal(payload)
 	if err != nil {
 		log.Fatalf("error marshalling payload: %v", err)
