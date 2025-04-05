a69da4d45f89fe45f21c2e1bbec6cdc4e09726c7
sreekara <sreedansd@gmail.com>
2025-04-05 02:21:53 -0700

    feature(car_metrics): added collection, repository, and use case to track metrics across cars (e.g. metrics for HT08 vs HT09).
    These metrics will track things like distance, battery usage and other data we want to
    track across individual runs.

---
+++ cloud_webserver_v2/internal/database/usecase/car_metrics_usecase.go
@@ -0,0 +1,31 @@
+package usecase
+
+import (
+	"context"
+
+	"github.com/hytech-racing/cloud-webserver-v2/internal/database/repository"
+	"github.com/hytech-racing/cloud-webserver-v2/internal/models"
+)
+
+type CarMetricsUseCase struct {
+	carMetricsRepo repository.CarMetricsRepository
+}
+
+func NewCarMetricsUseCase(carMetricsRepo repository.CarMetricsRepository) *CarMetricsUseCase {
+	return &CarMetricsUseCase{
+		carMetricsRepo,
+	}
+}
+
+func (uc *CarMetricsUseCase) CreateCarMetrics(ctx context.Context, model models.CarMetricsModel) (models.CarMetricsModel, error) {
+	resModel, err := uc.carMetricsRepo.Save(ctx, model)
+	if err != nil {
+		return models.CarMetricsModel{}, err
+	}
+
+	return resModel, nil
+}
+
+func (uc *CarMetricsUseCase) GetVechileRunById(ctx context.Context, id string) (*models.CarMetricsModel, error) {
+	return uc.carMetricsRepo.GetCarMetricsFromId(ctx, id)
+}
