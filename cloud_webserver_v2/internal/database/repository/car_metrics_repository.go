907eb6e464301db68b073e40336357cf02e1af08
sreekara <sreedansd@gmail.com>
2025-04-06 00:29:30 -0700

    feat(car_metrics): added ability to get all current car metrics
    Updated the car metrics interfaces to be more descriptive and accurate

--- cloud_webserver_v2/internal/database/repository/car_metrics_repository.go
+++ cloud_webserver_v2/internal/database/repository/car_metrics_repository.go
@@ -13,18 +13,22 @@ import (
 
 const CarMetricsModel string = "car_metrics"
 
+// CarMetricsRepository contains the methods any db implementation needs to implement to interact with car metrics data
 type CarMetricsRepository interface {
+	GetAllCarMetrics(ctx context.Context) ([]models.CarMetricsModel, error)
 	Save(ctx context.Context, carMetrics models.CarMetricsModel) (models.CarMetricsModel, error)
 	UpdateMetricByID(ctx context.Context, idStr string, metricUpdates models.CarMetricsModel) (models.CarMetricsModel, error)
 	GetCarMetricsFromId(ctx context.Context, idStr string) (*models.CarMetricsModel, error)
 }
 
+// MongoCarMetricsRepository conatins all the information needed to interact with a MongoDB implementation of the CarMetrics db
 type MongoCarMetricsRepository struct {
 	dbClient   *mongo.Client
 	db         *mongo.Database
 	collection *mongo.Collection
 }
 
+// NewMongoCarMetricsRepository creates a new NewMongoCarMetricsRepository with a MongoDB client and database
 func NewMongoCarMetricsRepository(dbClient *mongo.Client, database *mongo.Database) (*MongoCarMetricsRepository, error) {
 	collection := database.Collection(CarMetricsModel)
 	if collection == nil {
@@ -38,6 +42,9 @@ func NewMongoCarMetricsRepository(dbClient *mongo.Client, database *mongo.Databa
 	}, nil
 }
 
+// Save creates a new Car Metrics document in the collection.
+// For now, I see this being used rarely as the current plan is to create a new document for each car (HT08, Ht09, etc.)
+// May change if we want to target more granular iterations of each car
 func (repo *MongoCarMetricsRepository) Save(ctx context.Context, metrics models.CarMetricsModel) (models.CarMetricsModel, error) {
 	res, err := repo.collection.InsertOne(ctx, metrics)
 	if err != nil {
@@ -48,6 +55,7 @@ func (repo *MongoCarMetricsRepository) Save(ctx context.Context, metrics models.
 	return metrics, nil
 }
 
+// UpdateMetricByID updates a document with idStr and updates the contents of the document with data specified in CarMetricsModel
 func (repo *MongoCarMetricsRepository) UpdateMetricByID(ctx context.Context, idStr string, metricUpdates models.CarMetricsModel) (models.CarMetricsModel, error) {
 	updatedMetrics := models.CarMetricsModel{}
 	updateDoc, err := bson.Marshal(metricUpdates)
@@ -78,6 +86,7 @@ func (repo *MongoCarMetricsRepository) UpdateMetricByID(ctx context.Context, idS
 	return updatedMetrics, nil
 }
 
+// GetCarMetricsFromId gets a document from the MongoDB document with its idStr
 func (repo *MongoCarMetricsRepository) GetCarMetricsFromId(ctx context.Context, idStr string) (*models.CarMetricsModel, error) {
 	objID, err := primitive.ObjectIDFromHex(idStr)
 	if err != nil {
@@ -96,4 +105,19 @@ func (repo *MongoCarMetricsRepository) GetCarMetricsFromId(ctx context.Context, 
 	}
 
 	return &model, err
+}
+
+func (repo *MongoCarMetricsRepository) GetAllCarMetrics(ctx context.Context) ([]models.CarMetricsModel, error) {
+	filter := bson.M{}
+	cursor, err := repo.collection.Find(ctx, filter)
+	if err != nil {
+		return nil, err
+	}
+
+	var metrics []models.CarMetricsModel
+	if err = cursor.All(ctx, &metrics); err != nil {
+		return nil, err
+	}
+
+	return metrics, nil
 }
--- cloud_webserver_v2/internal/database/usecase/car_metrics_usecase.go
+++ cloud_webserver_v2/internal/database/usecase/car_metrics_usecase.go
@@ -26,6 +26,14 @@ func (uc *CarMetricsUseCase) CreateCarMetrics(ctx context.Context, model models.
 	return resModel, nil
 }
 
-func (uc *CarMetricsUseCase) GetVechileRunById(ctx context.Context, id string) (*models.CarMetricsModel, error) {
+func (uc *CarMetricsUseCase) GetCarMetricsById(ctx context.Context, id string) (*models.CarMetricsModel, error) {
 	return uc.carMetricsRepo.GetCarMetricsFromId(ctx, id)
+}
+
+func (uc *CarMetricsUseCase) GetAllCarMetrics(ctx context.Context) ([]models.CarMetricsModel, error) {
+	return uc.carMetricsRepo.GetAllCarMetrics(ctx)
+}
+
+func (uc *CarMetricsUseCase) UpdateCarMetrics(ctx context.Context, id string, updates models.CarMetricsModel) (models.CarMetricsModel, error) {
+	return uc.carMetricsRepo.UpdateMetricByID(ctx, id, updates)
 }
